c17f78f02f53956b2cea2eea0240656d8844626f
syuta-tM <63224909+syuta-tM@users.noreply.github.com>
2020-08-25 22:03:32 +0900

    main.php updated

--- main.php
+++ main.php
@@ -1,11 +1,34 @@
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="UTF-8">
-    <title>HTMLの書き方</title>
-  </head>
-  <body>
-    <h1>HTMLの書き方</h1>
-    <p>はじめてのHTMLを作りました</p>
-  </body>
-</html>
+<?php
+
+    //50個までの想定
+    for($i=0,$i<50,$i++){
+
+        //ファイルが無くなったら終了
+        //upname[i]はmain.htmlで書いていたもの　あっているか不明
+        if(file_exists("upname[i]")){
+            print '全てのファイルを読み込みました';
+            break;
+        }else{
+
+            //ファイル名を取得
+            $fileinfo[i] = "upname[i]";
+    
+            //ファイル名（拡張子を含まない）を取得 （使わなければ後で消す)
+            $filename[i] = pathinfo($pathinfo[i], PATHINFO_FILENAME);
+            //拡張子（ドットを含まない）
+            $extension[i] = pathinfo($pathinfo[i], PATHINFO_EXTENSION);
+
+            //ファイル名を変更する 
+            rename($fileinfo[i] , $newname $i"."$extension)
+            {
+                echo "変更できました"
+            }
+            else{
+                echo"失敗しました"
+            };
+        }
+    };
+
+    
+
+?>
