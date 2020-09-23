0731ddea30ddd6c01855be9caba42d169646a303
syuta-tM <63224909+syuta-tM@users.noreply.github.com>
2020-09-23 13:49:49 +0900

    changed
    changed

--- naming.php
+++ naming.php
@@ -1,4 +1,38 @@
 <?php
 
-//main.phpに書いてしまった
+    //50個までの想定
+    for($i=0,$i<50,$i++){
+
+        //ファイルが無くなったら終了
+        if(file_exists("upfilei]")){
+            print '全てのファイルを読み込みました';
+            break;
+        }else{
+
+            //ファイル名を取得
+            $fileinfo = "upfile[i]";
+    
+            //ファイル名（拡張子を含まない）を取得 （使わなければ後で消す)
+            $filename = pathinfo($pathinfo, PATHINFO_FILENAME);
+            //拡張子（ドットを含まない）
+            $extension = pathinfo($pathinfo, PATHINFO_EXTENSION);
+
+            //ファイル名を変更する 
+            if(rename($fileinfo , $NewName $i"."$extension))
+            {
+                echo "変更できました"
+            }
+            else{
+                echo"失敗しました"
+            };
+            
+            //初期化
+            $fileinfo = NULL;
+            $filename = NULL;
+            $extension = NULL;
+        }
+    };
+
+    
+
 ?>
