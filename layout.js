aec0637a9fc48a4f1a66ddf57928f81c2e674397
syuta-tM <63224909+syuta-tM@users.noreply.github.com>
2020-09-25 23:49:28 +0900

    changed

--- layout.js
+++ layout.js
@@ -9,18 +9,23 @@ FileStorage.click(function(){
 
 //ファイル名の表示のみ可能。複数個は不可
 $('#FileStorage').change(function() {
-	// フォームで選択された全ファイルを取得
-	
-	var file = $(this).prop('files')[0];
-    $('#layout-tile').html('ファイル名:' + file.name);
 
+	$('.layoutCanvas').hide();
+	
 	// 個数分の画像を表示する
-	for( var i=0,l=fileList.length; l>i; i++ ) {
+	var fileList = $(this).get(0).files.length;
+	for( var i=0,l=fileList; l>i; i++ ) {
+		var file = $(this).prop('files')[i];
+
+		$('#layout-tile').innerHTML += '<dev>' + file.name + '</div>' ;
+
+    	//$('#layout-tile').html('ファイル名:' + file.name);
+
 		// Blob URLの作成
-		var blobUrl = window.URL.createObjectURL( fileList[i] ) ;
+		/*var blobUrl = window.URL.createObjectURL( fileList[i] ) ;
 
 		// HTMLに書き出し (src属性にblob URLを指定)
-		$('#layout-tile').innerHTML += '<a href="' + blobUrl + '" FileStorage="_blank"><img src="' + blobUrl + '"></a>' ;
+		$('#layout-tile').innerHTML += '<a href="' + blobUrl + '" FileStorage="_blank"><img src="' + blobUrl + '"></a>' ;*/
 	}
 	
 	$('.layoutCanvas').hide();
