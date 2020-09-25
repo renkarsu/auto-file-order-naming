47dac7dc7a39869ccf2d9dbac7f4b2d10d6a0cd8
syuta-tM <63224909+syuta-tM@users.noreply.github.com>
2020-09-25 23:17:03 +0900

    changed

--- layout.js
+++ layout.js
@@ -1,11 +1,18 @@
 // <input type="file" name="upfile[]" multiple class="InputAddOn-field" id="FileStorage">にchangeイベントを設定
 
+//TEST用
+/*var FileStorage = $('#test-btn');
+FileStorage.click(function(){
+	alert("test");
+});*/
 
-/*var FileStorage = document.getElementById("FileStorage");
-FileStorage.addEventListener( "change", function() {
+
+//ファイル名の表示のみ可能。複数個は不可
+$('#FileStorage').change(function() {
 	// フォームで選択された全ファイルを取得
-    var fileList = this.files ;
-    $('.layoutCanvas').display('none');
+	
+	var file = $(this).prop('files')[0];
+    $('#layout-tile').html('ファイル名:' + file.name);
 
 	// 個数分の画像を表示する
 	for( var i=0,l=fileList.length; l>i; i++ ) {
@@ -15,8 +22,6 @@ FileStorage.addEventListener( "change", function() {
 		// HTMLに書き出し (src属性にblob URLを指定)
 		$('#layout-tile').innerHTML += '<a href="' + blobUrl + '" FileStorage="_blank"><img src="' + blobUrl + '"></a>' ;
 	}
-})*/
-
-$('#test-btn').click(function(){
-	alert("読み込みできました");
-})
+	
+	$('.layoutCanvas').hide();
+});
