06715884a0cdec71f0b6bb9e4de6f65fc23d5b55
syuta-tM <63224909+syuta-tM@users.noreply.github.com>
2020-09-27 18:42:17 +0900

    changed

--- layout.js
+++ layout.js
@@ -1,32 +1,55 @@
 // <input type="file" name="upfile[]" multiple class="InputAddOn-field" id="FileStorage">にchangeイベントを設定
 
-//TEST用
-/*var FileStorage = $('#test-btn');
-FileStorage.click(function(){
-	alert("test");
-});*/
-
+var THUMBNAIL_WIDTH = $('#layout-tile').Width / 5;
+var THUMBNAIL_HEIGHT = $('#layout-tile').height / 2;
 
 //ファイル名の表示のみ可能。複数個は不可
 $('#FileStorage').change(function() {
 
-	$('.layoutCanvas').hide();
+	$('.layoutCanvas').hide() ;
 	
 	// 個数分の画像を表示する
-	var fileList = $(this).get(0).files.length;
+	var fileList = $(this).get(0).files.length ;
 	for( var i=0,l=fileList; l>i; i++ ) {
-		var file = $(this).prop('files')[i];
+		var file = $(this).prop('files')[i] ;
 
-		$('#layout-tile').innerHTML += '<dev>' + file.name + '</div>' ;
+		if (! file.type.match('image.*')) {
+			reader.onload = function() {
+				var width, height;
+        		if(image.width > image.height){
+        			var ratio = image.height/image.width;
+            		width = THUMBNAIL_WIDTH;
+            		height = THUMBNAIL_WIDTH * ratio;
+        		}else if(image.height > image.width){
+            		var ratio = image.width/image.height;
+            		height = THUMBNAIL_HEIGHT;
+            		width = THUMBNAIL_HEIGHT * ratio;
+        		}
+				var img_src = "iamge.jpg";
+				$('#layout-tile').html(img_src);
+		  	}
+		}else{
+		  	// 画像表示
+		  	var reader = new FileReader();
+		  	reader.onload = function() {
+				var width, height;
+				if(image.width > image.height){
+					var ratio = image.height/image.width;
+					width = THUMBNAIL_WIDTH;
+					height = THUMBNAIL_WIDTH * ratio;
+				}else if(image.height > image.width){
+					var ratio = image.width/image.height;
+					height = THUMBNAIL_HEIGHT;
+					width = THUMBNAIL_HEIGHT * ratio;
+				}
+				var img_src = $('<img>').attr('src', reader.result);
+				$('#layout-tile').html(img_src);
+		  	}
+			reader.readAsDataURL(file);
+		}
 
-    	//$('#layout-tile').html('ファイル名:' + file.name);
-
-		// Blob URLの作成
-		/*var blobUrl = window.URL.createObjectURL( fileList[i] ) ;
-
-		// HTMLに書き出し (src属性にblob URLを指定)
-		$('#layout-tile').innerHTML += '<a href="' + blobUrl + '" FileStorage="_blank"><img src="' + blobUrl + '"></a>' ;*/
+		var fileName = file.name
+		$('#layout-tile').append('<dev>' + fileName + '</div>') ;
 	}
 	
-	$('.layoutCanvas').hide();
 });
