464548fc9aa6875a2d21c23806f5d81a158b19f5
syuta-tM <63224909+syuta-tM@users.noreply.github.com>
2020-10-08 22:52:51 +0900

    Thumbnail.js is completed
    サムネイルとファイル名の表示 , サムネイルのリサイズが可能

---
+++ Thumbnail.js
@@ -0,0 +1,21 @@
+document.getElementById( "FileStorage" ).onchange = function () {
+	$('.layoutCanvas').hide() ;
+	objectUrls = [] ;
+
+	var fileList = this.files ;
+
+	for( var i=0,l=fileList.length; l>i; i++ ) {
+		var objectUrl = URL.createObjectURL( fileList[i] ) ;
+		var fileName = fileList[i].name;
+
+		appendHTML( '<div><div>' + fileName + '<img src="' + objectUrl + '" width="100%"></div></div>' ) ;
+
+		objectUrls.push( objectUrl ) ;
+	}
+}
+
+function appendHTML ( html ) {
+	document.getElementById( "layout-tile" ).innerHTML += html ;
+}
+
+var objectUrls = [] ;
