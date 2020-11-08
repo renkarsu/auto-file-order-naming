a64fee7718fb53daf3d851fa9fea7c3934a4a5cd
syuta-tM <63224909+syuta-tM@users.noreply.github.com>
2020-11-08 16:52:13 +0900

    polish

--- scripts/download-naming.js
+++ scripts/download-naming.js
@@ -7,7 +7,7 @@ for (var i = 0, l = fileList.length; l > i; i++) {
     function download() {
         var element = document.createElement('a');
         element.setAttribute('href', img)
-        element.setAttribute('download', document.getElementById("cnno").value + '.png');
+        element.setAttribute('download', document.getElementById("fileName").value + document.getElementById("fileNumber").value + '.png');
 
         element.style.display = 'none';
         document.body.appendChild(element);
