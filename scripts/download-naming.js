11cc6771c820b2b53b7e996aa63c8660f2b90e29
syuta-tM <63224909+syuta-tM@users.noreply.github.com>
2020-11-13 17:23:32 +0900

    polish

--- scripts/download-naming.js
+++ scripts/download-naming.js
@@ -1,15 +1,23 @@
 function download() {
 
     var fileList = document.getElementById("FileStorage").files;
+
+    if (fileList.length == 0) {
+		alert('ファイルを選択してください');
+		return false;
+	}
+
     for (var i = 0, l = fileList.length; l > i; i++) {
 
         var img = URL.createObjectURL(fileList[i]);
         var number = document.getElementById("fileNumber").value;
         var currentNumber = parseInt(number, 10) + parseInt(i, 10);
 
+        
+
         var element = document.createElement('a');
         element.setAttribute('href', img)
-        element.setAttribute('download', document.getElementById("fileName").value + '_' +  currentNumber + '.png');
+        element.setAttribute('download', document.getElementById("fileName").value + '_' + currentNumber + '.png');
 
         element.style.display = 'none';
         document.body.appendChild(element);
