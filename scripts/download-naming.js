51baf508880c40e650af7944ffc91c7c4ba650d8
syuta-tM <63224909+syuta-tM@users.noreply.github.com>
2020-11-15 10:59:43 +0900

    Improve
    dow-nam.jsの機能追加
    0以上の数値にのみ適用
    拡張子を元のファイルに合わせる

--- scripts/download-naming.js
+++ scripts/download-naming.js
@@ -5,19 +5,31 @@ function download() {
     if (fileList.length == 0) {
 		alert('ファイルを選択してください');
 		return false;
-	}
+    }
+    
+    var number = document.getElementById("fileNumber").value;
+    if(number < 0){
+        alert('番号は0以上の数字を入力してください');
+        return false;
+    }
 
     for (var i = 0, l = fileList.length; l > i; i++) {
 
         var img = URL.createObjectURL(fileList[i]);
-        var number = document.getElementById("fileNumber").value;
         var currentNumber = parseInt(number, 10) + parseInt(i, 10);
+        var fileName = fileList[i].name;
 
-        
+        var ret
+        var fileTypes = fileName.split(".");
+        var len = fileTypes.length;
+        if(len === 0){
+            ret = "";
+        }
+        ret = fileTypes[len - 1];
 
         var element = document.createElement('a');
         element.setAttribute('href', img)
-        element.setAttribute('download', document.getElementById("fileName").value + '_' + currentNumber + '.png');
+        element.setAttribute('download', document.getElementById("fileName").value + '_' + currentNumber + '.' + ret);
 
         element.style.display = 'none';
         document.body.appendChild(element);
